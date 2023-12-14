c439ca8a16b77fe948f3e70f87012512f0859a61
Oleg Pavlovich <pavlovi4.o@gmail.com>
2023-12-14 15:55:13 +0300

    wip: adds pitch slope estimation utilities

--- utils/audio.py
+++ utils/audio.py
@@ -1,18 +1,20 @@
+import numpy as np
 import pyreaper
 
 from numpy.typing import NDArray
 from scipy.io import wavfile
+from scipy import stats
 
 
 MIN_F0 = 40
 MAX_F0 = 1100
 
 
-def read(wavpath: str, mmap: bool = False) -> tuple[int, NDArray]:
-    return wavfile.read(wavpath, mmap=mmap)
+def read(wavpath: str, mmap: bool = False) -> tuple[NDArray, int]:
+    return wavfile.read(wavpath, mmap=mmap)[::-1]
 
 
-def compute_f0(rate: int, audio: NDArray, freq: float = 0.01) -> tuple[NDArray, NDArray]:
+def compute_pitch(audio: NDArray, rate: int, freq: float = 0.01) -> tuple[NDArray, NDArray]:
     """Computes pitch (F0) values for the given audio.
 
     Returns:
@@ -28,3 +30,11 @@ def compute_f0(rate: int, audio: NDArray, freq: float = 0.01) -> tuple[NDArray, 
         unvoiced_cost=0.9
     )
     return outs[2:4]
+
+
+def compute_pitch_slope(f0: NDArray) -> float:
+    x = np.arange(len(f0))
+    mask = f0 > 0
+    if len(f0[mask]):
+        return stats.linregress(x[mask], f0[mask])[0]
+    return 0
